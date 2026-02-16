e4b92ce444a8bf1d2be9c515c7ff83a6e91160ea
hasan-shkoukani <hasanshukukani@gmail.com>
2026-02-17 00:44:18 +0200

    added a project

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -169,6 +169,11 @@ function Container() {
                         "smart assistant web app that helps users generate realistic shopping/meal plans based on a budget OR estimate the total cost of a plan they've already made. It uses Google Gemini AI to intelligently handle budget estimation and plan generation.",
                     github: "https://github.com/Hasan-Shkoukani/spendless",
                     },{
+                    name: "Paradise Nursery",
+                    description:
+                        "a mini ecommerce website that uses redux.js for the store, it was submitted and accepted by IBM in the React.js Course",
+                    github: "https://github.com/Hasan-Shkoukani/Paradise-Nursery",
+                    },{
                     name: "Arduino LED Rgb Strip Animations",
                     description:
                         "Arduino LED RGB Strip, with 2 animations, using C code and Tinker CAD website",
